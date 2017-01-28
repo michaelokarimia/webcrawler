4a2e0a25e7c20c0138ba6a3ab92beb09c2371481
Michael Okarimia <git@michaelokarimia.com>
2017-01-28 19:37:28 +0000

    Webcrawler command line application

---
+++ webcrawler.go
@@ -0,0 +1,12 @@
+package main
+
+import (	"fmt"
+	"github.com/michaelokarimia/crawlerclient"
+)
+
+func main (){
+  fmt.Printf("Starting crawlerclient")
+  domain:= "michaelokarimia.com"
+  output := crawlerclient.Crawlerclient(domain)
+  fmt.Printf(output)
+}
