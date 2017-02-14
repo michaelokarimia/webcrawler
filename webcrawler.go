ae69e21b6e91a37097e75a0f45e8cfca739fd2c9
Michael Okarimia <git@michaelokarimia.com>
2017-02-14 13:04:54 +0000

    Accepts domain as cmd line arg, basic validation

--- webcrawler.go
+++ webcrawler.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/michaelokarimia/crawlerclient"
 )
 
 func main() {
 	fmt.Printf("Starting crawlerclient")
-	domain := "michaelokarimia.com"
-	output := crawlerclient.GetUrls(domain)
-	fmt.Printf(output)
+
+	if len(os.Args) == 2 {
+		domain := os.Args[1]
+
+		fmt.Printf("\nCrawling domain: %s", domain)
+
+		output := crawlerclient.GetUrls(domain)
+		fmt.Printf("\n" + output + "\n")
+	} else {
+		fmt.Printf("\nUsage: argument must be a valid domain, i.e example.com \n")
+	}
 }
